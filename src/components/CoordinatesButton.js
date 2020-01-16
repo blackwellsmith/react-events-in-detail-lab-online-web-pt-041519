262b182c55166e1d4936787acf6d5af3773b01c9
blackwellsmith <blackwellsmithiv@gmail.com>
2020-01-16 11:29:17 -0500

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
-// Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+
+export default class DelayedButton extends Component{
+
+    handleClick = event => {
+        console.log(this)
+        this.props.onReceiveCoordinates([event.clientX, event.clientY]);
+    }
+    
+    render() {
+        return <button onClick={this.handleClick}>Coords</button>
+    }
+    
+} 
